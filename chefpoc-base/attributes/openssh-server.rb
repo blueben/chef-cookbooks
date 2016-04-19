a49b53b546de5f0366be462b4d552778ce44dd73
Benjamin Krueger <benjamink@slalom.com>
2016-04-19 16:47:19 -0700

    Recipe rename

--- chefpoc-base/attributes/openssh-server.rb
+++
@@ -1,4 +0,0 @@
-default.openssh.server.permit_root_login = 'no'
-default.openssh.server.password_authentication = 'no'
-default.openssh.server.allow_groups = 'sudo'
-default.openssh.server.print_motd = 'no'
