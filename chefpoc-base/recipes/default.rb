b1fa2754abcac33ac0399fcef1e48cf5fe0e34f1
Benjamin Krueger <benjamink@slalom.com>
2016-04-19 18:29:54 -0700

    Adding all recipes to base default

--- chefpoc-base/recipes/default.rb
+++ chefpoc-base/recipes/default.rb
@@ -3,3 +3,14 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 Slalom Consulting, All Rights Reserved.
+
+include_recipe 'chefpoc-base::chef-client'
+include_recipe 'chefpoc-base::cron'
+include_recipe 'chefpoc-base::firewall'
+include_recipe 'chefpoc-base::ntp'
+include_recipe 'chefpoc-base::openssh'
+include_recipe 'chefpoc-base::packages'
+include_recipe 'chefpoc-base::postfix'
+include_recipe 'chefpoc-base::rsyslog'
+include_recipe 'chefpoc-base::selinux'
+include_recipe 'chefpoc-base::sysctl'
