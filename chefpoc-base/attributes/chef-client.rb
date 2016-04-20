9a132793ac2d2e97fd9869cf1473f33e71dd9595
Benjamin Krueger <benjamink@slalom.com>
2016-04-19 17:04:25 -0700

    Initial chef-client recipe attributes

---
+++ chefpoc-base/attributes/chef-client.rb
@@ -0,0 +1,4 @@
+default.chef_client.cron.mailto = "benjamink@slalom.com"
+default.chef_client.config.ssl_verify_mode = :verify_none
+default.chef_client.config.client_fork = true
+default.chef_client.config.enable_reporting = true
