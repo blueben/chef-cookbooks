e1f6c24e9a98317809d59226189648664d11e2b3
Benjamin Krueger <benjamink@slalom.com>
2016-04-19 17:18:26 -0700

    Initial firewall recipe

---
+++ chefpoc-base/attributes/firewall.rb
@@ -0,0 +1,3 @@
+default.firewall.allow_established = true
+default.firewall.ipv6_enabled = false
+default.firewall.firewalld.permanent = true
--- chefpoc-base/recipes/firewall.rb
+++ chefpoc-base/recipes/firewall.rb
@@ -3,3 +3,5 @@
 # Recipe:: firewall
 #
 # Copyright (c) 2016 Slalom Consulting, All Rights Reserved.
+
+include_recipe 'firewall'
